3b0d8e915cb70ff3a690f95f0d416d22a80ac2a6
Alan Berberov <alan.almaco67@gmail.com>
2023-05-08 16:13:44 +0200

    [ADD] constructor to Gene<>;

--- Heredity/Assets/_Scripts/Gene.cs
+++ Heredity/Assets/_Scripts/Gene.cs
@@ -3,4 +3,10 @@ public class Gene<T>
 {
     public AllelePair Dom;
     public TraitRelation<T> Relation;
+
+    public Gene(AllelePair domination, TraitRelation<T> relation)
+    {
+        Dom = domination;
+        Relation = relation;
+    }
 }
