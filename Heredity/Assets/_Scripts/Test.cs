7a3c94139bdc7e43eb0f315bc09bc2a6ac1fd2f0
Alan Berberov <alan.almaco67@gmail.com>
2023-05-08 18:15:11 +0200

    [ADD] Test script to test inheratance;

---
+++ Heredity/Assets/_Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public bool test;
+    public ColorGenotype Genotype;
+    public List<Gene<Color>> a;
+    public List<Gene<Color>> b;
+
+
+    private void OnValidate()
+    {
+        if (!test) return;
+        
+        Genotype.Initialize(a, b);
+        test = false;
+    }
+}
